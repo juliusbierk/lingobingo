24194b25aa6a1b044ac2c819008d40c0b03c41a0
Julius Bier Kirkegaard <juliusbierk@gmail.com>
2021-11-24 13:53:57 +0100

    Update algo.py

--- algo.py
+++ algo.py
@@ -7,6 +7,10 @@ def create_card(n_card_words, n_words, earliest_win):
     idxs = np.arange(n_words)
     subtract = 1 + max(0, np.random.poisson(n_card_words * (n_words - earliest_win) / n_words) - 1)
     subtract = min(subtract, n_card_words - 2)
+    
+    if len(idxs[earliest_win:]) < substract:
+        substract = len(idxs[earliest_win:])
+    
     first = np.random.choice(idxs[:earliest_win], size=n_card_words - subtract, replace=False)
     last = np.random.choice(idxs[earliest_win:], size=subtract, replace=False)
     return np.random.permutation(np.append(first, last))
